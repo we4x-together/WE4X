33ecfc10624d1bd478635d6fc4e2b2b7b41ad692
Shivansh Kushwaha <shivanshkushwaha518@gmail.com>
2026-08-06 20:22:58 +0530

    Initial project upload

---
+++ client/src/pages/Layout.jsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import { Outlet } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { loadTheme } from "../features/themeSlice";
+import { Loader2Icon } from "lucide-react";
+import {
+  useUser,
+  SignIn,
+  useAuth,
+  CreateOrganization,
+} from "@clerk/clerk-react";
+import { fetchWorkspaces } from "../features/workspaceSlice";
+
+const Layout = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const { loading, workspaces } = useSelector((state) => state.workspace);
+  const dispatch = useDispatch();
+  const { user, isLoaded } = useUser();
+  const { getToken } = useAuth();
+
+  // Initial load of theme
+  useEffect(() => {
+    dispatch(loadTheme());
+  }, []);
+
+  // Initial load of workspaces
+  useEffect(() => {
+    if (isLoaded && user && workspaces.length === 0) {
+      dispatch(fetchWorkspaces({ getToken }));
+    }
+  }, [user, isLoaded]);
+
+  if (!user) {
+    return (
+      <div className="flex justify-center items-center h-screen bg-white dark:bg-zinc-950">
+        <SignIn />
+      </div>
+    );
+  }
+
+  if (loading)
+    return (
+      <div className="flex items-center justify-center h-screen bg-white dark:bg-zinc-950">
+        <Loader2Icon className="size-7 text-blue-500 animate-spin" />
+      </div>
+    );
+
+  if (user && workspaces.length === 0) {
+    return (
+      <div className="min-h-screen flex justify-center items-center">
+        <CreateOrganization />
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex bg-white dark:bg-zinc-950 text-gray-900 dark:text-slate-100">
+      <Sidebar
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <div className="flex-1 flex flex-col h-screen">
+        <Navbar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        <div className="flex-1 h-full p-6 xl:p-10 xl:px-16 overflow-y-scroll">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ server/controllers/workspaceController.js
@@ -0,0 +1,94 @@
+import prisma from "../configs/prisma.js";
+
+// Get all workspaces for user
+export const getUserWorkspaces = async (req, res) => {
+  try {
+    const { userId } = await req.auth();
+    const workspaces = await prisma.workspace.findMany({
+      where: {
+        members: { some: { userId: userId } },
+      },
+      include: {
+        members: { include: { user: true } },
+        projects: {
+          include: {
+            tasks: {
+              include: {
+                assignee: true,
+                comments: { include: { user: true } },
+              },
+            },
+            members: { include: { user: true } },
+          },
+        },
+        owner: true,
+      },
+    });
+    res.json({ workspaces });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.code || error.message });
+  }
+};
+
+// Add member to workspace
+
+export const addMember = async (req, res) => {
+  try {
+    const { userId } = await req.auth();
+    const { email, role, workspaceId, message } = req.body;
+
+    //check if user exists
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    if (!workspaceId || !role) {
+      return res.status(400).json({ message: "Missing required parameters" });
+    }
+    if (!["ADMIN", "MEMBER"].includes(role)) {
+      return res.status(400).json({ message: "Invalid role" });
+    }
+    //fetch workspace
+    const workspace = await prisma.workspace.findUnique({
+      where: { id: workspaceId },
+      include: { members: true },
+    });
+
+    if (!workspace) {
+      return res.status(404).json({ message: "Workspace not found" });
+    }
+    // check creator has admin role
+    if (
+      !workspace.members.find(
+        (member) => member.userId === userId && member.role === "ADMIN",
+      )
+    ) {
+      return res
+        .status(401)
+        .json({ message: "You do not have admin privileges" });
+    }
+    // check if user is already a member
+    const existingMember = workspace.members.find(
+      (member) => member.userId === userId,
+    );
+
+    if (existingMember) {
+      return res.status(400).json({ message: "User is already a member" });
+    }
+
+    const member = await prisma.workspaceMember.create({
+      data: {
+        userId: user.id,
+        workspaceId,
+        role,
+        message,
+      },
+    });
+    res.json({ member, message: "Member added successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.code || error.message });
+  }
+};
